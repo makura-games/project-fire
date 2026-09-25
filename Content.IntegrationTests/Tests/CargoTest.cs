72b165960edf4033a783914cc2238dd35644543a
deleteddata271-ship-it <deleteddata271@gmail.com>
2026-09-25 17:53:07 +0300

    пробуем починить

--- Content.IntegrationTests/Tests/CargoTest.cs
+++ Content.IntegrationTests/Tests/CargoTest.cs
@@ -19,11 +19,11 @@ namespace Content.IntegrationTests.Tests;
 [TestFixture]
 public sealed class CargoTest
 {
-    private static readonly HashSet<ProtoId<CargoProductPrototype>> Ignored =
-    [
+//    private static readonly HashSet<ProtoId<CargoProductPrototype>> Ignored =
+//    [
         // This is ignored because it is explicitly intended to be able to sell for more than it costs.
-        new("FunCrateGambling")
-    ];
+//        new("FunCrateGambling")
+//    ];
 
     [Test]
     public async Task NoCargoOrderArbitrage()
